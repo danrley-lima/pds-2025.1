f760b636aea39f97a4b7265de7ecd33995fa84c3
Djavan <djavancosta93@gmail.com>
2025-07-19 13:31:28 -0300

    fix: controller

--- product-api/src/main/java/com/danrley/product_management/domain/grocery/controller/GroceryPromotionConditionController.java
+++ product-api/src/main/java/com/danrley/product_management/domain/grocery/controller/GroceryPromotionConditionController.java
@@ -1,5 +1,26 @@
 package com.danrley.product_management.domain.grocery.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.danrley.product_management.common.dto.promotion.PromotionResponseDTO;
+import com.danrley.product_management.common.dto.promotionCondition.PromotionConditionRequestDTO;
+import com.danrley.product_management.core.controller.BasePromotionConditionController;
+import com.danrley.product_management.domain.grocery.model.GroceryProduct;
+import com.danrley.product_management.domain.grocery.model.GroceryPromotion;
+import com.danrley.product_management.domain.grocery.model.GroceryPromotionCondition;
+import com.danrley.product_management.domain.grocery.service.GroceryPromotionConditionService;
+import com.danrley.product_management.domain.grocery.service.GroceryPromotionService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController
 @RequestMapping("/api/grocery/promotions")
 public class GroceryPromotionConditionController extends
